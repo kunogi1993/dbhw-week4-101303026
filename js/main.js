a8e32a1dd0787f1847e509c288b358f3281af224
Weiting <101303026@nccu.edu.tw>
2016-04-07 15:23:02 +0800

    git add .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://opendata.khcc.gov.tw/public/OD_culturalbus_busscene.ashx";
+var API_PATH = "http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=1&FileType=1&Lang=C&FolderType=U";
 
 function getData(cb) {
   $.ajax({
